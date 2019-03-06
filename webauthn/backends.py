f885cf6b58665c9009ed662d6155673650a124c8
Boris Shemigon <i@boris.co>
2019-03-06 15:49:23 -0500

    model storage backend

--- webauthn/backends.py
+++ webauthn/backends.py
@@ -1,56 +1,42 @@
-from dataclasses import dataclass
+from django.contrib.auth import get_user_model
 
 from pywarp import Credential
-from pywarp.backends import CredentialStorageBackend
+from pywarp.backends import AbstractStorageBackend
+from webauthn.models import WebAuthnUser
 
 
-@dataclass
-class User:
-    email: str
-    credential_id: bytes = None
-    credential_public_key: bytes = None
-    registration_challenge: bytes = None
-    authentication_challenge: bytes = None
+class ModelBackend(AbstractStorageBackend):
+    def __init__(self):
+        self.user_model = get_user_model()
 
-
-class SessionBackend(CredentialStorageBackend):
-    def __init__(self, request):
-        self.session = request.session
-        self.key = '__webauthn__%s'
-
-    def get_credential_by_email(self, email):
-        user = self.get_user(email)
+    def get_credential(self, username):
+        user = self.user_model.objects.get_by_natural_key(username).webauthn
         return Credential(credential_id=user.credential_id,
                           credential_public_key=user.credential_public_key)
 
-    def save_credential_for_user(self, email, credential):
-        self.save(User(
-            email=email,
-            credential_id=credential.id,
-            credential_public_key=bytes(credential.public_key)
-        ))
+    def save_credential(self, username, credential, **user_extra):
+        user = self.user_model.objects.get_by_natural_key(username).webauthn
+        user.credential_id = credential.id
+        user.credential_public_key = bytes(credential.public_key)
+        user.save()
 
-    def save_challenge_for_user(self, email, challenge, type):
-        assert type in {"registration", "authentication"}
+    def save_challenge(self, username, challenge, challenge_type):
+        assert challenge_type in {"registration", "authentication"}
 
-        user = self.get_user(email)
-        setattr(user, type + "_challenge", challenge)
-        self.save(user)
+        if challenge_type == 'registration':
+            WebAuthnUser.objects.create(
+                user=self.user_model.objects.create(**{
+                    self.user_model.USERNAME_FIELD: username
+                }),
+                registration_challenge=challenge,
+            )
+        else:
+            user = self.user_model.objects.get_by_natural_key(username).webauthn
+            user.authentication_challenge = challenge
+            user.save()
 
-    def get_challenge_for_user(self, email, type):
-        assert type in {"registration", "authentication"}
-        user = self.get_user(email)
-        return getattr(user, type + "_challenge")
+    def get_challenge(self, username, challenge_type):
+        assert challenge_type in {"registration", "authentication"}
 
-    def get_user(self, email):
-        try:
-            user = self.session[self.key % email]
-            if user.email == email:
-                return user
-        except KeyError:
-            pass
-        return User(email=email)
-
-    def save(self, user: User):
-        assert user.email is not None
-        self.session[self.key % user.email] = user
+        user = self.user_model.objects.get_by_natural_key(username).webauthn
+        return getattr(user, challenge_type + "_challenge")
